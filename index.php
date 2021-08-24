266bcb656fd84b0bb93e8893a7280007d0593d12
Jacob Flores <42787753+JacobGT@users.noreply.github.com>
2021-08-23 20:03:43 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<html>
+<meta charset="UTF-8">
+<meta name="Cyber Dagger" content="Examen parcial 1.">
+        <meta charset="UTF-8">
+        <meta name="description" content="Elementos de Bootstrap y JQuery aplicados al sitio web y empresa Cyber Dagger.">
+        <meta name="keywords" content="HTML, CSS, JavaScript, Cyber Dagger, Progra IV">
+        <meta name="author" content="Jacob Flores, Rodrigo Leon">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <title>Parcial 1 Progra IV</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico?v=2">
+  </head>
+  <body style="background-color: #ccffcc">
+    <?php echo '<h1>Ejemplos Bootstrap & JQuery</h1>'; ?> 
+    <hr/>
+    <p>A continuacion se mostraran y explicaran 40 elementos: 20 elementos de Bootstrap y 20 elementos de JQuery. Esta pagina de inicio no se personalizó mucho ni se usó CSS para poder contrastar la diferencia de un archivo html con un archivo html que usa CSS y JS.</p>
+    <br/>
+
+    <p> 
+      <h3>Bootstrap</h3>
+      Ejemplos:
+      <ol>
+        <li><a href="bootstrap_examples/sign_in.php"> Sign-In</a></li>
+        <li><a href="bootstrap_examples/sidebars.php"> Side Bars</a></li>
+        <li><a href="bootstrap_examples/footers.php"> Footers</a></li>
+        <li><a href="bootstrap_examples/header.php"> Header</a></li>
+        <li><a href="bootstrap_examples/heroes.php"> Heroes</a></li>
+        <li><a href="bootstrap_examples/features.php"> Features</a></li>
+        <li><a href="bootstrap_examples/drop_downs.php"> Drop Downs</a></li>
+        <li><a href="bootstrap_examples/checkout.php"> Checkout</a></li>
+        <li><a href="bootstrap_examples/cover.php"> Cover</a></li>
+        <li><a href="bootstrap_examples/pricing.php"> Pricing</a></li>
+        <li><a href="bootstrap_examples/dashboard.php"> Dashboard</a></li>
+        <li><a href="bootstrap_examples/product.php"> Product</a></li>
+        <li><a href="bootstrap_examples/album.php"> Album</a></li>
+        <li><a href="bootstrap_examples/jumbotron.php"> Jumbotron</a></li>
+        <li><a href="bootstrap_examples/grid.php"> Grid</a></li>
+        <li><a href="bootstrap_examples/offcanvas_navbar.php"> OffCanvas NavBar</a></li>
+        <li><a href="bootstrap_examples/blog.php"> Blog</a></li>
+        <li><a href="bootstrap_examples/cheat_sheet.php"> Cheat Sheet</a></li>
+        <li><a href="bootstrap_examples/starter_template.php"> Starter Template</a></li>
+        <li><a href="bootstrap_examples/modals.php"> Modals</a></li>
+      </ol>
+    </p>
+    
+    <hr/>
+    
+    <p>
+      <h3>JQuery</h3>
+        Ejemplos:
+        <ol>
+        <li><a href="jquery_examples/animation_datepicker.php"> Animation Datepicker</a></li>
+        <li><a href="jquery_examples/animation_dialogs.php"> Animation Dialogs</a></li>
+        <li><a href="jquery_examples/icon_menu.php"> Icon Menu</a></li>
+        <li><a href="jquery_examples/collapse_accordion.php"> Collapse Accordion</a></li>
+        <li><a href="jquery_examples/icon_button.php"> Icon Button</a></li>
+        <li><a href="jquery_examples/categories_autocomplete.php"> Categories Auto Complete</a></li>
+        <li><a href="jquery_examples/range_slider.php"> Range Slider</a></li>
+        <li><a href="jquery_examples/color_slider.php"> Color Slider</a></li>
+        <li><a href="jquery_examples/vertical_slider.php"> Vertical Slider</a></li>
+        <li><a href="jquery_examples/product_checkbox.php"> Product Check Box</a></li>
+        <li><a href="jquery_examples/currency_spinner.php">Currency Spinner</a></li>
+        <li><a href="jquery_examples/time_spinner.php"> Time Spinner</a></li>
+        <li><a href="jquery_examples/split_control_group.php"> Split Control Group</a></li>
+        <li><a href="jquery_examples/download_progressbar.php"> Download Progress Bar</a></li>
+        <li><a href="jquery_examples/custom_rendering_selectmenu.php"> Custom Rendering Select Menu</a></li>
+        <li><a href="jquery_examples/product_selection_selectmenu.php"> Product Selection Select Menu</a></li>
+        <li><a href="jquery_examples/form_tooltip.php"> Form Tool Tip</a></li>
+        <li><a href="jquery_examples/track_the_mouse_tooltip.php"> Track the Mouse Tool Tip</a></li>
+        <li><a href="jquery_examples/vertical_tabs.php"> Vertical Tabs</a></li>
+        <li><a href="jquery_examples/open_on_mouseover_tabs.php"> Open on MouseOver Tabs</a></li>
+      </ol>
+    </p>
+    </br>
+  </body>
+</html>
